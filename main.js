3c2d353d8568407bcc53a22895f5cb8a4a71f9d4
Tony <un_razum@mail.ru>
2026-07-30 02:40:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 new WOW().init();
+// var mySwiper = new Swiper('.swiper-container', {
+//     speed: 400,
+//     spaceBetween: 100
+// })
+
+const swiper = new Swiper('.swiper', {
+ speed: 400,
+ spaceBetween:100,
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass : 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true
+  },
+ 
+})
+
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+// //   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+// //   navigation: {
+// //     nextEl: '.swiper-button-next',
+// //     prevEl: '.swiper-button-prev',
+// //   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
